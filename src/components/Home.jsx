7398d1bcd5ee48b3c3d371a42a834642d74929ea
reiji11011 <reiji.sugama@gmail.com>
2021-09-21 21:54:22 +0900

    [add]index.jsを用いて情報をindex.htmlに出力を行う

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return <div>Home</div>;
+    }
+}
+
+// exportすることによって、他のクラスでimportして使えるようになる。
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/Home.jsx';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Homeコンポーネントをindex.htmlの<div id="root"></div>に表示する。
+ReactDOM.render(<Home />, document.getElementById('root'));
